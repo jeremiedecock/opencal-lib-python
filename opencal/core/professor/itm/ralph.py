0d5621e690eec8cc0546ff64c5c989875ee55f42
Jeremie DECOCK <jd.jdhp@gmail.com>
2019-09-01 13:47:40 +0200

    Add Ralph ITM professor (random selection of cards with replacement).

---
+++ opencal/core/professor/itm/ralph.py
@@ -0,0 +1,25 @@
+"""Ralph randomly pick cards. No matter the answer, each card is put back in the stack.
+This professor is used for intermediate-term memory training.
+"""
+
+import random
+
+class ProfessorRalph:
+
+    def __init__(self, card_list):
+        self._card_list = [card for card in card_list if not card["hidden"]]
+        self.update_card()
+
+    def update_card(self):
+        if len(self._card_list) > 0:
+            current_index = random.randint(0, len(self._card_list) - 1)
+            self._current_card = self._card_list[current_index]
+        else:
+            self._current_card = None
+
+    @property
+    def current_card(self):
+        return self._current_card
+
+    def current_card_reply(self, answer, hide=False, duration=None, confidence=None):
+        self.update_card()

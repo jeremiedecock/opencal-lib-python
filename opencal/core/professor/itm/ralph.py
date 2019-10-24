018a8ad55f9c19044ae8cdf0c649741c79b643f8
Jeremie DECOCK <jd.jdhp@gmail.com>
2019-10-24 23:18:09 +0200

    Misc updates.

--- opencal/core/professor/itm/ralph.py
+++ opencal/core/professor/itm/ralph.py
@@ -8,17 +8,17 @@ class ProfessorRalph:
 
     def __init__(self, card_list):
         self.update_card_list(card_list)
-
-    def update_card(self):
-        if len(self._card_list) > 0:
-            current_index = random.randint(0, len(self._card_list) - 1)
-            self._current_card = self._card_list[current_index]
-        else:
-            self._current_card = None
+        self._current_card = None
 
     @property
     def current_card(self):
         return self._current_card
+
+    def update_card(self):
+        if len(self._card_list) > 0:
+            self._current_card = random.choice(self._card_list)
+        else:
+            self._current_card = None
 
     def current_card_reply(self, answer, hide=False, duration=None, confidence=None):
         self.update_card()

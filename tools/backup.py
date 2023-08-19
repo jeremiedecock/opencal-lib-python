a4dbc6054770689b0d830064e6cb993d835adf7c
Jeremie DECOCK <jd.jdhp@gmail.com>
2023-08-19 09:58:29 +0200

    Add a script to make a backup of the new SQLite database and anotherone to dump this SQLite database.

---
+++ tools/backup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Src: https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection.backup
+
+import opencal.io.database
+
+opencal.io.database.backup_db()
---
+++ tools/dump.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Convert SQLite DB file to SQL dump file dump.sql
+# Src: https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection.iterdump
+
+import opencal.io.database
+
+opencal.io.database.dump_db()

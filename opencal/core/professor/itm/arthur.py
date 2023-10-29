57f06130a275788935f758a336ada02d2f8cfc30
Jeremie DECOCK <jd.jdhp@gmail.com>
2023-10-30 00:28:27 +0100

    Add new ITM professors.

--- opencal/core/professor/itm/arthur.py
+++ opencal/core/professor/itm/arthur.py
@@ -1,4 +1,4 @@
-"""Professor Arthur pick cards in an "active set" that is progressively widened.
+"""Professor Arthur pick cards in an "work in progress" window that is progressively moved along the full cards list.
 For each right answer, the card is removed.
 This professor is used for intermediate-term memory training.
 """
@@ -6,50 +6,68 @@ This professor is used for intermediate-term memory training.
 from opencal.core.professor.professor import AbstractProfessor
 from opencal.core.data import RIGHT_ANSWER_STR, WRONG_ANSWER_STR
 
-DEFAULT_ACTIVE_LIST_INCREMENT_SIZE = 5
-
-# TODO : passer la constante prec dans le fichier de config YAML
+DEFAULT_CARDS_IN_PROGRESS_INCREMENT_SIZE = 5
+DEFAULT_RIGHT_ANSWERS_RATE_THRESHOLD = 0.5
 
 class ProfessorArthur(AbstractProfessor):
 
-    def __init__(self, card_list, active_list_increment_size=DEFAULT_ACTIVE_LIST_INCREMENT_SIZE):
+    def __init__(self, card_list, cards_in_progress_increment_size=DEFAULT_CARDS_IN_PROGRESS_INCREMENT_SIZE, right_answers_rate_threshold=DEFAULT_RIGHT_ANSWERS_RATE_THRESHOLD):
         super().__init__()
 
-        self.active_list_increment_size = active_list_increment_size
+        self._cards_not_yet_reviewed_list = []
+        self._cards_in_progress_list = []
+        self._num_right_answers = []          # the total number of right answers for each card
+        self._num_wrong_answers = []          # the total number of wrong answers for each card
+
+        self.cards_in_progress_increment_size = cards_in_progress_increment_size
+        self.right_answers_rate_threshold = right_answers_rate_threshold
         self.update_card_list(card_list)
 
 
     @property
     def current_card(self):
-        if len(self._active_card_list) == 0:
+        if len(self._cards_in_progress_list) == 0:
             print()
-            if self._active_card_list_size < len(self._full_card_list):
-                # The active card list is incremented
-                self._active_card_list_size += self.active_list_increment_size
-                self._active_card_list = self._full_card_list[:self._active_card_list_size]
-                print("Widen the active card list ; current size = {:3d} ".format(len(self._active_card_list)), end='', flush=True)
+            if self._last_card_in_progress_index < len(self._cards_not_yet_reviewed_list):
+                # Update the list of cards being reviewed ("cards in progress")
+                reviewed_cards_list = self._cards_not_yet_reviewed_list[:self._last_card_in_progress_index]
+                self._cards_in_progress_list = self._cards_not_yet_reviewed_list[self._last_card_in_progress_index:self._last_card_in_progress_index+self.cards_in_progress_increment_size]
+                self._last_card_in_progress_index += self.cards_in_progress_increment_size
+
+                # Add to self._cards_in_progress_list the less well known cards from reviewed_cards_list
+                for (card_index, card) in reviewed_cards_list:
+                    right_answers_rate = self._num_right_answers[card_index] / (self._num_right_answers[card_index] + self._num_wrong_answers[card_index])
+                    if right_answers_rate < self.right_answers_rate_threshold:
+                        self._cards_in_progress_list.append((card_index, card))
+                print(f"Update the work in progress card list ; current index = {self._last_card_in_progress_index} ; len cards_in_progress_list = { len(self._cards_in_progress_list) }", end='', flush=True)
             else:
-                # review is completed
+                # Review is completed
                 print("Review completed")
                 return None
 
-        return self._active_card_list[0]
+        return self._cards_in_progress_list[0][1]
 
 
     def current_card_reply(self, answer, hide=False, duration=None, confidence=None):
-        if len(self._active_card_list) > 0:
-            card = self._active_card_list.pop(0)
+        if len(self._cards_in_progress_list) > 0:
+            # Pick the first card in progress
+            (card_index, card) = self._cards_in_progress_list.pop(0)
 
             if answer in ("skip", WRONG_ANSWER_STR):
-                self._active_card_list.append(card)
+                # If the answer is right or skip, put the card back to the end of the cards in progress list
+                self._num_wrong_answers[card_index] += 1
+                self._cards_in_progress_list.append((card_index, card))
             elif answer == RIGHT_ANSWER_STR:
+                self._num_right_answers[card_index] += 1
                 print(".", end='', flush=True)
             else:
                 raise ValueError("Unknown answer : {}".format(answer))
 
 
     def update_card_list(self, card_list):
-        self._full_card_list = [card for card in card_list if not card["hidden"]]
-        self._active_card_list = []
-        self._active_card_list_size = 0
+        self._cards_not_yet_reviewed_list = [(card_index, card) for (card_index, card) in enumerate(card_list) if not card["hidden"]]
+        self._cards_in_progress_list = []
+        self._num_right_answers = [0 for card in card_list if not card["hidden"]]          # the total number of right answers for each card
+        self._num_wrong_answers = [0 for card in card_list if not card["hidden"]]          # the total number of wrong answers for each card
+        self._last_card_in_progress_index = 0
         print("Review {:d} cards".format(len(card_list)))
---
+++ opencal/core/professor/itm/ernest.py
@@ -0,0 +1,58 @@
+"""Professor Ernest pick cards in an "work in progress" window that is progressively moved along the full cards list.
+For each right answer, the card is removed.
+This professor is used for intermediate-term memory training.
+"""
+
+from opencal.core.professor.professor import AbstractProfessor
+from opencal.core.data import RIGHT_ANSWER_STR, WRONG_ANSWER_STR
+
+CARDS_IN_PROGRESS_INCREMENT_SIZE = 5
+
+class ProfessorErnest(AbstractProfessor):
+
+    def __init__(self, card_list, cards_in_progress_increment_size=CARDS_IN_PROGRESS_INCREMENT_SIZE):
+        super().__init__()
+
+        self._cards_not_yet_reviewed_list = []
+        self._cards_in_progress_list = []
+
+        self.cards_in_progress_increment_size = cards_in_progress_increment_size
+        self.update_card_list(card_list)
+
+
+    @property
+    def current_card(self):
+        if len(self._cards_in_progress_list) == 0:
+            print()
+            if self._last_card_in_progress_index < len(self._cards_not_yet_reviewed_list):
+                # Update the list of cards being reviewed ("cards in progress")
+                self._cards_in_progress_list = self._cards_not_yet_reviewed_list[self._last_card_in_progress_index:self._last_card_in_progress_index+self.cards_in_progress_increment_size]
+                self._last_card_in_progress_index += self.cards_in_progress_increment_size
+                print(f"Update the work in progress card list ; current index = {self._last_card_in_progress_index} ", end='', flush=True)
+            else:
+                # Review is completed
+                print("Review completed")
+                return None
+
+        return self._cards_in_progress_list[0]
+
+
+    def current_card_reply(self, answer, hide=False, duration=None, confidence=None):
+        if len(self._cards_in_progress_list) > 0:
+            # Pick the first card in progress
+            card = self._cards_in_progress_list.pop(0)
+
+            if answer in ("skip", WRONG_ANSWER_STR):
+                # If the answer is right or skip, put the card back to the end of the cards in progress list
+                self._cards_in_progress_list.append(card)
+            elif answer == RIGHT_ANSWER_STR:
+                print(".", end='', flush=True)
+            else:
+                raise ValueError("Unknown answer : {}".format(answer))
+
+
+    def update_card_list(self, card_list):
+        self._cards_not_yet_reviewed_list = [card for card in card_list if not card["hidden"]]
+        self._cards_in_progress_list = []
+        self._last_card_in_progress_index = 0
+        print("Review {:d} cards".format(len(card_list)))
